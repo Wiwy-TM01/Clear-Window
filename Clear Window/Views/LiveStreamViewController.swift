279e00b389d7732bc8b9c0e3d9da782757d01849
Edward Barber <25310324+Wiwy-TM01@users.noreply.github.com>
2020-10-16 17:45:09 -0400

    - Establish a player view class - Center Video while in landscape / portrait - Prepare to move logic to PlayerView class

--- Clear Window/Views/LiveStreamViewController.swift
+++ Clear Window/Views/LiveStreamViewController.swift
@@ -7,31 +7,38 @@
 
 import UIKit
 import AVKit
-import AVFoundation
 
 class LiveStreamViewController: UIViewController {
+    
+    private let playerLayer = PlayerView()
     
     @IBOutlet weak var playerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpPlayer()
+    }
+    
+    private func setUpPlayer() {
         guard let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/clear-window.appspot.com/o/coverr-drone-shot-in-tierra-del-fuego-argentina-6-4247.mp4?alt=media&token=4c3a625b-f0ed-4202-aae7-0cc665514ed1")
         else {
             return
         }
         
         let player = AVPlayer(url: url)
-        let controller = AVPlayerLayer()
         
-        controller.player = player
+        playerLayer.player = player
         
-        controller.frame = playerView.bounds
-        controller.videoGravity = .resizeAspect
-
-        playerView.layer.addSublayer(controller)
+        playerView.addSubview(playerLayer)
         
-        player.play()
+        playerLayer.translatesAutoresizingMaskIntoConstraints = false
+        playerLayer.leadingAnchor.constraint(equalTo: playerView.leadingAnchor).isActive = true
+        playerLayer.trailingAnchor.constraint(equalTo: playerView.trailingAnchor).isActive = true
+        playerLayer.heightAnchor.constraint(equalTo: playerView.widthAnchor, multiplier: 16/9).isActive = true
+        playerLayer.centerYAnchor.constraint(equalTo: playerView.centerYAnchor).isActive = true
+        
+        playerLayer.player?.play()
     }
-    
+
 }
---
+++ Clear Window/Views/PlayerView.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerView.swift
+//  Clear Window
+//
+//  Created by Edward A. Barber on 10/16/20.
+//
+
+import UIKit
+import AVKit
+
+class PlayerView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    // Override UIView property
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
